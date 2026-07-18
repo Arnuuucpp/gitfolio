07a17b36230b3ed2a53e3873c9ed63c07f0d4446
Arnav <itsarnav3546@gmail.com>
2026-07-18 14:32:40 +0530

    Add navbar and hero section with neubrutalist styling, integrate Remix Icons and custom fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import Axios from 'axios'
-import Card from './Components/Card'
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import Card from "./Components/Card";
+import Navbar from "./Components/Navbar";
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/500.css";
+import "@fontsource/space-grotesk/700.css";
+import "@fontsource/archivo-black";
 
 const App = () => {
-  const [Username, setUsername] = useState('')
-  const [UserData, setUserData] = useState(null)
-  const [repo, setrepo] = useState(null)
+  const [Username, setUsername] = useState("");
+  const [UserData, setUserData] = useState(null);
+  const [repo, setrepo] = useState(null);
 
-  const getData = async ()=>{
+  const getData = async () => {
     try {
-      const response = await Axios.get(`https://api.github.com/users/${Username}`)
-      const repo = await Axios.get(`https://api.github.com/users/${Username}/repos`)
+      // const response = await Axios.get(`https://api.github.com/users/${Username}`)
+      // const repo = await Axios.get(`https://api.github.com/users/${Username}/repos`)
       // console.log(response.data)
-      console.log(repo.data[2].name)
+      console.log(repo.data[2].name);
 
-      setUserData(response.data)
-      setrepo(repo.data)
+      setUserData(response.data);
+      setrepo(repo.data);
     } catch (error) {
-      console.log("User not found or API error", error)
+      console.log("User not found or API error", error);
     }
-  }
+  };
 
   return (
-    <div className='bg-black h-screen p-10'>
-      <h2 className='text-2xl text-white'>GitFolio</h2>
-      <input type="text" 
-      className='border-2 border-amber-200 mr-2 p-2 rounded-2xl mt-4 text-white'
-      value={Username}
-      onChange={(e)=>{
-        // console.log(e.target.value)
-        setUsername(e.target.value)
-      }}
-      />
-      <button className='p-2 bg-amber-200 font-bold rounded-2xl border-white border-2 cursor-pointer'
-      onClick={getData}
-      >GetData</button>
+    <div className="bg-[#F5F5F5] h-screen p-4">
+      <Navbar />
 
-      <div className='mt-6'>
-        <Card UserData={UserData} repo={repo}/>
+      <div className="p-10 flex flex-col items-center justify-center">
+        <h1 className="font-archivo text-5xl text-center text-zinc-800">
+          find any developer on github...
+        </h1>
+        <div className="flex items-center justify-center gap-5 mt-6 mb-3">
+          <div className="relative">
+            <i className="ri-search-line absolute left-4 top-1/2 -translate-y-1/2 text-black text-lg"></i>
+            <input
+              type="text"
+              placeholder="enter the username"
+              className="
+            <RiSearch2Line />
+            font-grotesk text-sm w-125 bg-transparent border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] px-10 py-3 text-black font-semibold focus:outline-none cursor-text"
+              value={Username}
+              onChange={(e) => {
+                // console.log(e.target.value)
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+          <button
+            className="font-grotesk text-sm w-fit bg-[#FF6B4A] border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] px-10 py-3 text-black font-semibold focus:outline-none cursor-pointer active:scale-95"
+            onClick={getData}
+          >
+            Find Now!
+          </button>
+        </div>
+      </div>
+
+      <div className="mt-6">
+        {/* <Card UserData={UserData} repo={repo}/> */}
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex items-center justify-between px-2 py-2 border-b-2 border-black">
+      <div className="flex items-center gap-2">
+        <i className="ri-github-fill text-3xl text-black hover:scale-110 cursor-pointer"></i>
+        <span className="font-archivo text-xl text-black">GITFOLIO</span>
+        <p class="text-black font-sans text-sm font-normal  inline-block absolute left-16 top-14">
+          Browse users and their profiles via <a target="blank"
+            href="https://developer.github.com/v3/"
+            class="text-[#269930] no-underline hover:underline transition-colors duration-200"
+          >
+            the GitHub API
+          </a>
+        </p>
+      </div>
 
-export default Navbar
+      <div className="flex items-center gap-8">
+        <span className="font-grotesk text-sm text-black cursor-pointer whitespace-nowrap hover:font-bold">
+          Compare
+        </span>
+        <span className="font-grotesk text-sm text-black cursor-pointer hover:font-bold">
+          Liked
+        </span>
+        <span className="text-black">|</span>
+
+        <button className="font-grotesk text-sm w-full bg-[#8fbdf8] border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] px-10 py-3 text-black font-semibold focus:outline-none cursor-pointer active:scale-95">
+          Login
+        </button>
+
+        <button className="font-grotesk text-sm w-full bg-[#8fbdf8] border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] px-10 py-3 text-black font-semibold focus:outline-none cursor-pointer active:scale-95">
+          Signup
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
